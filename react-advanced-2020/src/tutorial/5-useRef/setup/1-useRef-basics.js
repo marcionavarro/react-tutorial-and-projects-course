0f8cbecaff7b706944b2d865e2c1d5f0a95ca755
marcionavarro <marcionavarrodearaujo@gmail.com>
2023-09-03 15:02:32 -0300

    React Hooks and Advanced Topics

---
+++ react-advanced-2020/src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useRef } from 'react';
+
+// preserves value
+// DOES NOT trigger re-render
+// target DOM nodes/elements
+
+const UseRefBasics = () => {
+  const refContainer = useRef(null);
+  const divContainer = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(refContainer.current.value);
+    console.log(divContainer.current);
+  };
+
+  useEffect(() => {
+    console.log(refContainer.current);
+    refContainer.current.focus();
+  });
+
+  return (
+    <>
+      <form action='' className='form' onClick={handleSubmit}>
+        <input type='text' ref={refContainer} />
+        <button type='submit'>Submit</button>
+      </form>
+      <div ref={divContainer}>hell world</div>
+    </>
+  );
+};
+
+export default UseRefBasics;
