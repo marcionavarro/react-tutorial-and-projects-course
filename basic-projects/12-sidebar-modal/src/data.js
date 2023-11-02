f3d094463a0af2ca18b3c8de2ea16103fdf69ecb
marcionavarro <marcionavarrodearaujo@gmail.com>
2023-11-02 12:17:51 -0300

    12-Sidebar Modal - Structure

--- basic-projects/12-sidebar-modal/src/data.js
+++ basic-projects/12-sidebar-modal/src/data.js
@@ -9,65 +9,65 @@ import {
   FaUserFriends,
   FaFolderOpen,
   FaCalendarAlt,
-  FaWpforms,
+  FaWpforms
 } from 'react-icons/fa';
 export const links = [
   {
     id: 1,
     url: '/',
     text: 'home',
-    icon: <FaHome />,
+    icon: <FaHome />
   },
   {
     id: 2,
     url: '/team',
     text: 'team',
-    icon: <FaUserFriends />,
+    icon: <FaUserFriends />
   },
   {
     id: 3,
     url: '/projects',
     text: 'projects',
-    icon: <FaFolderOpen />,
+    icon: <FaFolderOpen />
   },
   {
     id: 4,
     url: '/calendar',
     text: 'calendar',
-    icon: <FaCalendarAlt />,
+    icon: <FaCalendarAlt />
   },
   {
     id: 5,
     url: '/documents',
     text: 'documents',
-    icon: <FaWpforms />,
-  },
+    icon: <FaWpforms />
+  }
 ];
 
 export const social = [
   {
     id: 1,
     url: 'https://www.twitter.com',
-    icon: <FaFacebook />,
+    icon: <FaFacebook />
   },
   {
     id: 2,
     url: 'https://www.twitter.com',
-    icon: <FaTwitter />,
+    icon: <FaTwitter />
   },
   {
     id: 3,
     url: 'https://www.twitter.com',
-    icon: <FaLinkedin />,
+    icon: <FaLinkedin />
   },
   {
     id: 4,
     url: 'https://www.twitter.com',
-    icon: <FaBehance />,
+    icon: <FaBehance />
   },
   {
     id: 5,
     url: 'https://www.twitter.com',
-    icon: <FaSketch />,
-  },
+    icon: <FaSketch />
+  }
 ];
