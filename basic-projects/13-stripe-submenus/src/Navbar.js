ea88a215040cff3337637a55e0c41af1908175cc
marcionavarro <marcionavarrodearaujo@gmail.com>
2023-11-03 09:49:00 -0300

    13-Stripe - Submenu

--- basic-projects/13-stripe-submenus/src/Navbar.js
+++ basic-projects/13-stripe-submenus/src/Navbar.js
@@ -6,6 +6,11 @@ import { useGlobalContext } from './context';
 const Navbar = () => {
   const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
 
+  const displaySubmenu = () => {
+    console.log('hello world');
+    openSubmenu();
+  };
+
   return (
     <nav className='nav'>
       <div className='nav-center'>
@@ -17,16 +22,24 @@ const Navbar = () => {
         </div>
         <ul className='nav-links'>
           <li>
-            <button className='link-btn'>products</button>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
+              products
+            </button>
           </li>
           <li>
-            <button className='link-btn'>developers</button>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
+              developers
+            </button>
           </li>
           <li>
-            <button className='link-btn'>company</button>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
+              company
+            </button>
           </li>
         </ul>
-        <button className='btn signin-btn'>Sign in</button>
+        <button className='btn signin-btn' onMouseOver={displaySubmenu}>
+          Sign in
+        </button>
       </div>
     </nav>
   );
