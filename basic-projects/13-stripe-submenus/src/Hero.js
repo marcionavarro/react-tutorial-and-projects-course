8d88d9599b82a6a91bb4fb5d7f5c6dca04ff1310
marcionavarro <marcionavarrodearaujo@gmail.com>
2023-11-03 09:31:36 -0300

    13-Stripe - Hero

--- basic-projects/13-stripe-submenus/src/Hero.js
+++ basic-projects/13-stripe-submenus/src/Hero.js
@@ -3,9 +3,27 @@ import phoneImg from './images/phone.svg';
 import { useGlobalContext } from './context';
 
 const Hero = () => {
-  const data = useGlobalContext();
-  console.log(data);
-  return <h2>hero component</h2>;
+  const { closeSubmenu } = useGlobalContext();
+
+  return (
+    <section className='hero'>
+      <div className='hero-center'>
+        <article className='hero-info'>
+          <h1>Infraestrutura de pagamentos para a internet</h1>
+          <p>
+            Milhões de empresas de todos os tamanhos usam a Stripe online e
+            presencialmente para receber pagamentos, enviar repasses,
+            automatizar processos financeiros e ampliar suas receitas. Comece
+            agora Fale com nossa equipe
+          </p>
+          <button className='btn'>Comece agora</button>
+        </article>
+        <article className='hero-images'>
+          <img src={phoneImg} alt='phone' className='phone-img' />
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
