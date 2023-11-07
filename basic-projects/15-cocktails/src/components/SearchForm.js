e5cc70a12111a4327a5d3749f1c51032a9ee20d7
marcionavarro <marcionavarrodearaujo@gmail.com>
2023-11-06 23:04:21 -0300

    15-Cocktails - Context API Setup,  Fetch Drinks

--- basic-projects/15-cocktails/src/components/SearchForm.js
+++ basic-projects/15-cocktails/src/components/SearchForm.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import { useGlobalContext } from '../context'
+import React from 'react';
+import { useGlobalContext } from '../context';
+import Loading from './Loading';
 
 const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext();
   return (
     <div>
       <h2>search form component</h2>
     </div>
-  )
-}
+  );
+};
 
-export default SearchForm
+export default SearchForm;
