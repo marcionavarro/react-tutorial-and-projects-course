80321f18f9e05971fb856658ebbfc53f488cac3f
marcionavarro <marcionavarrodearaujo@gmail.com>
2023-11-06 22:37:18 -0300

    15-Cocktails - About and Error

--- basic-projects/15-cocktails/src/pages/About.js
+++ basic-projects/15-cocktails/src/pages/About.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>Sobre Nós</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium ea
+        accusantium consequatur qui quidem reprehenderit impedit adipisci
+        consequuntur aut voluptas velit esse, saepe hic ipsa odio mollitia
+        laboriosam provident. Magni at quae dicta aperiam perferendis facere,
+        consequatur, consequuntur libero possimus saepe eveniet doloribus.
+        Deleniti est suscipit a beatae nulla sed.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
