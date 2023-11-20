7610028d8b6410666275e62ee840248c3a4972e2
marcionavarro <marcionavarrodearaujo@gmail.com>
2023-11-20 11:04:38 -0300

    04-stock-photos -  Scroll Event, Infinite Scroll, Search Query

--- Additional Projects (API and External Libraries)/04-stock-photos/src/App.js
+++ Additional Projects (API and External Libraries)/04-stock-photos/src/App.js
@@ -9,15 +9,32 @@ const searchUrl = `https://api.unsplash.com/search/photos/`;
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
 
   const fetchImages = async () => {
     setLoading(true);
     let url;
-    url = `${mainUrl}${clientID}`;
+    const urlPage = `&page=${page}`;
+    const urlQuery = `&query=${query}`;
+
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`;
+    }
+
     try {
       const response = await fetch(url);
       const data = await response.json();
-      setPhotos(data);
+      console.log(data);
+      setPhotos((oldPhotos) => {
+        if(query){
+          return [...oldPhotos, ...data.results];
+        }else{
+          return [...oldPhotos, ...data];
+        }
+      });
       setLoading(false);
     } catch (error) {
       setLoading(false);
@@ -27,18 +44,41 @@ function App() {
 
   useEffect(() => {
     fetchImages();
+  }, [page]);
+
+  useEffect(() => {
+    const event = window.addEventListener("scroll", () => {
+      /* console.log(`innerHeight ${window.innerHeight}`);
+      console.log(`scrollY ${window.scrollY}`);
+      console.log(`body height ${document.body.scrollHeight}`); */
+      if (
+        !loading &&
+        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
+      ) {
+        setPage((oldPage) => {
+          return oldPage + 1;
+        });
+      }
+    });
+    return () => window.removeEventListener("scroll", event);
   }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello");
+    fetchImages();
   };
 
   return (
     <main>
       <section className="search">
         <form action="" className="search-form">
-          <input type="text" placeholder="Pesquisar" className="form-input" />
+          <input
+            type="text"
+            placeholder="Pesquisar"
+            className="form-input"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
           <button type="submit" className="submit-btn" onClick={handleSubmit}>
             <FaSearch />
           </button>
@@ -46,8 +86,8 @@ function App() {
       </section>
       <section className="photos">
         <div className="photos-center">
-          {photos.map((image) => {
-            return <Photo key={image.id} {...image} />
+          {photos.map((image, index) => {
+            return <Photo key={image.id} {...image} />;
           })}
         </div>
         {loading && <h2 className="loading">Loading...</h2>}
