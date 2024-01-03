5150237acd5a2c24eeb4ec9a63faaab7dfcba60f
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-01-02 23:33:23 -0300

    react-github-search-users - Fusion Charts API, Fusion Charts - Configuration Options, Fusion Charts - Pie Chart, Calculate Most Used Language

--- react-github-search-users/src/components/Charts/Pie3D.js
+++ react-github-search-users/src/components/Charts/Pie3D.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { Component } from "react";
+import FusionCharts from "fusioncharts";
+import chart from "fusioncharts/fusioncharts.charts";
+import Charts from "fusioncharts/fusioncharts.charts";
+import ReactFC from "react-fusioncharts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
 
-const Pie3D = () => {
-  return <div>chart</div>;
+ReactFC.fcRoot(FusionCharts, chart, FusionTheme);
+
+const ChartComponent = ({data}) => {
+
+  const chartConfigs = {
+    type: "pie3d",
+    width: 400,
+    height: 400,
+    dataFormat: "json",
+    dataSource: {
+      chart: {
+        caption: "Languages",
+        theme: "fusion",
+        decimals:0,
+        pieRadius:"45%",
+        // paletteColors: '#F0DB4F'
+      },
+      data
+    }
+  };
+
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default Pie3D;
+export default ChartComponent;
