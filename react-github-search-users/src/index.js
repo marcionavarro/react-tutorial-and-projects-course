9e7a0c52a7665b12b5833dfd298a48ede5a21e2d
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-01-06 01:48:11 -0300

    react-github-search-users - Finalizado

--- react-github-search-users/src/index.js
+++ react-github-search-users/src/index.js
@@ -15,6 +15,7 @@ root.render(
       domain={process.env.REACT_APP_DOMAIN}
       clientId={process.env.REACT_APP_CLIENT_ID}
       redirectUri={window.location.origin}
+      cacheLocation="localstorage"
     >
       <GithubProvider>
         <App />
