fccf2b625697db592e6d16097b2890d3d0b826de
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-01-22 23:44:26 -0300

    store - Error Page,  About, Checkout Page,  Home Page - Hero

--- store/src/pages/AboutPage.js
+++ store/src/pages/AboutPage.js
@@ -1,11 +1,31 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from 'react';
+import styled from 'styled-components';
+import { PageHero } from '../components';
+import aboutImg from '../assets/hero-bcg.jpeg';
+import { GiMartini } from 'react-icons/gi';
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title='About' />
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='nice desk' />
+        <article>
+          <div className='title'>
+            <h2>our story</h2>
+            <div className='underline'></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At quam
+            voluptates iste cum dignissimos numquam amet minus modi, natus
+            voluptatum consectetur asperiores suscipit ad illum. Eius ex animi
+            illo corporis.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +53,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
