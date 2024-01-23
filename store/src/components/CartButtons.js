014a6d102ae1a4ba200b6c5d4b815b48334e2a13
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-01-22 23:15:16 -0300

    store -  Sidebar, Products Context Setup, Sidebar Toggle

--- store/src/components/CartButtons.js
+++ store/src/components/CartButtons.js
@@ -7,14 +7,15 @@ import { useCartContext } from '../context/cart_context';
 import { useUserContext } from '../context/user_context';
 
 const CartButtons = () => {
+  const { closeSidebar } = useProductsContext();
   return (
     <Wrapper className='cart-btn-wrapper'>
-      <Link to='/cart' className='cart-btn'>
+      <Link to='/cart' className='cart-btn' onClick={closeSidebar}>
         Cart
         <span className='cart-container'>
           <FaShoppingCart />
+          <span className='cart-value'>12</span>
         </span>
-        <span className='cart-value'>12</span>
       </Link>
       <button type='button' className='auth-btn'>
         Login <FaUserPlus />
