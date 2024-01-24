44ecc6db2f54550cd459338506987b2c0fbef9b4
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-01-24 01:13:02 -0300

    store -  Filters - Categories, Filters - Companies, Filters - Colors, Filters - Price, Filters - Shipping and Clear Filters

--- store/src/reducers/filter_reducer.js
+++ store/src/reducers/filter_reducer.js
@@ -12,6 +12,7 @@ import {
 const filter_reducer = (state, action) => {
   if (action.type === LOAD_PRODUCTS) {
     let maxPrice = action.payload.map((p) => p.price);
+    maxPrice = Math.max(...maxPrice);
 
     return {
       ...state,
@@ -73,8 +74,22 @@ const filter_reducer = (state, action) => {
   }
 
   if (action.type === FILTER_PRODUCTS) {
-    console.log('filtering products');
     return { ...state };
+  }
+
+  if (action.type === CLEAR_FILTERS) {
+    return {
+      ...state,
+      filters: {
+        ...state.filters,
+        text: '',
+        company: 'all',
+        category: 'all',
+        color: 'all',
+        price: state.filters.max_price,
+        shipping: false
+      }
+    };
   }
 
   throw new Error(`No Matching "${action.type}" - action type`);
